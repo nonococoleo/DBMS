cd6915403d26cbd5a8a162196851454fead3b1d8
nono <leo212121@foxmail.com>
2018-05-12 19:56:49 +0800

    remove files

--- application/news/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//配置文件
-return [
-    // 默认控制器名
-    'default_controller'     => 'Qjwb',
-    // 默认操作名
-    'default_action'         => 'ser'
-];
--- public/library/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=118.89.191.235;charset=utf8;dbname=library", "leo", "leo212121");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException$e) {
-    die("Error!:" . $e->getMessage() . "<br/>");
-}
