93a4eca4f511a30b453570e82fbc9026c1eda70f
diamond <1015678620@qq.com>
2018-05-14 17:33:02 +0800

    增，改学生表

--- application/tlr/controller/Student.php
+++ application/tlr/controller/Student.php
@@ -14,18 +14,64 @@ class Student extends Controller
     }
 
     public function students(Request $request) {
-        if (!isset($_GET)) {
+        if (!request()->isGet()) {
             $this->error("404 not found");
             exit();
         }
         $page = (isset($_GET['page']))? $_GET['page'] : 1;
-        $studentModel = new StudentModel;
-        $students = $studentModel->page($page,10)->select();
+        $student = new StudentModel;
+        $students = $student->page($page,10)->select();
         $totolPage = ceil(db('student')->count()/10);
         echo json_encode(array("students" => $students,"totolPage"=>$totolPage, "success" => true));
     }
 
-    public function new_student(Request $request) {
-        echo "111";
+    public function update(Request $request) {
+        if (!request()->isPost() || empty($_POST)) {
+            $this->error("404 not found");
+            exit();   
+        }
+
+        if ($_POST['sname']=='' || !isset($_POST['sname'])){
+            echo json_encode(array("success"=>false,'msg'=>"学生姓名不能为空"));
+            exit();   
+        }
+        if ($_POST['phone']=='' || $_POST['phone']==null){
+            echo json_encode(array("success"=>false,'msg'=>"手机号码不能为空"));
+            exit();   
+        }
+
+        $student = new StudentModel;
+        if($student->allowField(['sname','sex','grade','school','home','tel','phone','memo'])->save($_POST, ['sid' => $_POST['sid']])) {
+            echo json_encode(array("success"=>true));
+            exit();
+        } else {
+            echo json_encode(array("success"=>false,'msg'=>"服务器繁忙，请稍后重试"));
+            exit();
+        }
+    }
+
+    public function add(Request $request) {
+        if (!request()->isPost() || empty($_POST)) {
+            $this->error("404 not found");
+            exit();   
+        }
+
+        if ($_POST['sname']=='' || !isset($_POST['sname'])){
+            echo json_encode(array("success"=>false,'msg'=>"学生姓名不能为空"));
+            exit();   
+        }
+        if ($_POST['phone']=='' || !isset($_POST['phone'])){
+            echo json_encode(array("success"=>false,'msg'=>"手机号码不能为空"));
+            exit();   
+        }
+
+        $student = new StudentModel($_POST);
+        if($student->allowField(['sname','sex','grade','school','home','tel','phone','memo'])->save($_POST)) {
+            echo json_encode(array("success"=>true));
+            exit();
+        } else {
+            echo json_encode(array("success"=>false,'msg'=>"服务器繁忙，请稍后重试"));
+            exit();
+        }
     }
 }
