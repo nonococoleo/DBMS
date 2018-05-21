d5ece08c94506b749fe93be697db6c75219c490c
nono <leo212121@foxmail.com>
2018-05-21 16:09:39 +0800

    teacher查询完成

--- application/tlr/controller/Teacher.php
+++ application/tlr/controller/Teacher.php
@@ -2,6 +2,7 @@
 
 namespace app\tlr\controller;
 
+use app\tlr\model\TeacherModel;
 use think\Controller;
 use think\Request;
 
@@ -9,9 +10,30 @@ class Teacher extends Controller
 {
     public function index(Request $request)
     {
-        $name = 'index';
-        $this->assign(['name' => $name]);
+        $Teacher = new TeacherModel();
+        $teacher = $Teacher->paginate(10, false, ['type' => 'bootstrap']);
+        $page = $teacher->render();
+        // 向V层传数据
+        $data = ["teacher" => $teacher, "page" => $page];
+        $this->assign($data);
         $htmls = $this->fetch('index');
+        return $htmls;
+    }
+
+    public function search(Request $request)
+    {
+        $Teacher = new TeacherModel();
+        $name = $request->param('name');
+        $school = $request->param('school');
+        $query = $request->param();
+        $teacher = $Teacher->where('tname', 'like', "%$name%")->where('school', 'like', "%$school%")->order('tid', 'asc')->paginate(10, false, ['type' => 'bootstrap', 'query' => $query]);
+        $page = $teacher->render();
+        // 向V层传数据
+        $data = ["teacher" => $teacher, "page" => $page];
+        $this->assign($data);
+
+        // 取回打包后的数据
+        $htmls = $this->fetch("index");
         return $htmls;
     }
 }
