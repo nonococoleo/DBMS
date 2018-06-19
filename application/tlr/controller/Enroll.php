61d128053094a5c26d6c4b157a116855ccbb1a0f
nono <leo212121@foxmail.com>
2018-06-19 17:36:11 +0800

    enroll start

--- application/tlr/controller/Enroll.php
+++ application/tlr/controller/Enroll.php
@@ -2,7 +2,6 @@
 
 namespace app\tlr\controller;
 
-use app\tlr\model\EnrollModel;
 use think\Controller;
 use think\Request;
 
@@ -10,12 +9,12 @@ class Enroll extends Controller
 {
     public function index(Request $request)
     {
-        $Enroll = new EnrollModel();
-        $enroll = $Enroll->paginate(10, false, ['type' => 'bootstrap']);
-        $page = $enroll->render();
-
-        $data = ["enroll" => $enroll, "page" => $page, "name" => null];
-        $this->assign($data);
+//        $Enroll = new EnrollModel();
+//        $enroll = $Enroll->paginate(10, false, ['type' => 'bootstrap']);
+//        $page = $enroll->render();
+//
+//        $data = ["enroll" => $enroll, "page" => $page, "name" => null];
+//        $this->assign($data);
         $htmls = $this->fetch('index');
         return $htmls;
     }
