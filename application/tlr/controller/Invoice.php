e6d954bf3a4029d45c08397de738dcea5e7f151a
xuyang <496936914@qq.com>
2018-06-22 14:26:06 +0800

    invoice

---
+++ application/tlr/controller/Invoice.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\tlr\controller;
+
+use app\tlr\model\InvoiceModel;
+use think\Controller;
+use think\Request;
+use gmars\rbac\Rbac;
+
+class Invoice extends Controller
+{
+    // 发票页面
+    public function index(Request $request)
+    {
+        return $this->fetch(invoices'index');
+    }
+
+    // 获取发票列表
+    public function (Request $request)
+    {
+        if (!request()->isGet()) {
+            $this->error("404 not found");
+            exit();
+        }
+        $page = (isset($_GET['page'])) ? $_GET['page'] : 1;
+        $invoice = new InvoiceModel;
+        $invoices = $invoice->page($page, 10)->select();
+        $totalPage = ceil(db('invoice')->count() / 10);
+        echo json_encode(array("invoices" => $invoices, "totalPage" => $totalPage, "success" => true));
+    }
+
+
+}
