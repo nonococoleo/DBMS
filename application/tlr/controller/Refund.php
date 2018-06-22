d327f9fc2016bdadad6c613d0c5cf2954143b2e6
Beliefree <1365875821@qq.com>
2018-06-22 10:15:13 +0800

    test

---
+++ application/tlr/controller/Refund.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\tlr\controller;
+
+use app\tlr\model\TeacherModel;
+use think\Controller;
+use think\Request;
+
+class Refund extends Controller
+{
+    public function index(Request $request)
+    {
+        echo "2111";
+    }
+}
