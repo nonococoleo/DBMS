2d3796b995b4d5c0293de77bf66ec7328fa9dadc
nono <leo212121@foxmail.com>
2018-06-26 10:27:26 +0800

    some improvement

--- application/tlr/controller/Index.php
+++ application/tlr/controller/Index.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace app\tlr\controller;
+
+use app\tlr\model\SemesterModel;
 use app\tlr\model\UserModel;
+use gmars\rbac\Rbac;
 use think\Controller;
 use think\Request;
 use think\Session;
-use gmars\rbac\Rbac;
 
 class Index extends Controller
 {
@@ -20,12 +22,14 @@ class Index extends Controller
 
     public function login(Request $request)
     {
-    	return $this->fetch('login');
+        return $this->fetch('login');
     }
 
     public function loginHandle(Request $request)
     {
         $User = new UserModel();
+        $Semester = new SemesterModel();
+        $semester = $Semester->where("current", '=', 1)->order("id", "desc")->limit(1)->select();
         $data = array(
             'uname' => $_POST['uname'],
             'passwd' => md5($_POST['passwd'])
@@ -38,19 +42,20 @@ class Index extends Controller
             $rbacObj = new Rbac();
             $rbacObj->cachePermission($user['uid']);
             $rbacObj->cacheRole($user['uid']);
+            Session::set('cur_semester', $semester[0]->id);
             Session::set('uid', $user['uid']);
             Session::set('uname', $user['uname']);
-            $this->success("登录成功", "Index/index");
+            $this->success("登录成功", "Index/index", null, 1);
         } else{
             $this->error("用户名或密码错误");
         }
     }
 
     public function logOut(Request $request)
-    {   
+    {
         if(Session::get('uid')){
             session_destroy();
-            $this->success('安全退出',url('Index/login'));    
+            $this->success('安全退出',url('Index/login'));
         }else{
             $this->error('操作无效');
         }
