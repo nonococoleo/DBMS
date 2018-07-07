6a0de309daf3d4a69cf253a2845c9cfb83218dcb
diamond <1015678620@qq.com>
2018-07-07 18:34:45 +0800

    change something

--- application/tlr/controller/Index.php
+++ application/tlr/controller/Index.php
@@ -13,11 +13,12 @@ class Index extends Controller
 {
     public function index(Request $request)
     {
-        $name = 'index';
-        $stu = "./" . url("tlr/student");
-        $this->assign(['name' => $name, 'stu' => $stu]);
-        $htmls = $this->fetch('index');
-        return $htmls;
+        // $name = 'index';
+        // $stu = "./" . url("tlr/student");
+        // $this->assign(['name' => $name, 'stu' => $stu]);
+        // $htmls = $this->fetch('index');
+        // return $htmls;
+        return $this->fetch('login');
     }
 
     public function login(Request $request)
